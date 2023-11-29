2da4922552163ddcb7bc3a14e086402cce948b22
Greg Todd <gregtodd89@gmail.com>
2023-11-29 14:19:07 -0800

    created addition program allowing sum of 2 numbers

---
+++ addition.py
@@ -0,0 +1,10 @@
+def addition(a, b):
+    return a + b
+
+# Main Program
+num1 = float(input('Enter your 1st number:\n'))
+num2 = float(input('Enter your 2nd number:\n'))
+
+# Calling our function
+result = addition(num1, num2)
+print('The result is', result)
